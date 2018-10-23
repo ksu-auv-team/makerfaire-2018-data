f4adb1ddc58e2bd0e53372f0dcd2fd34598a3fc7
cmcdermitt <charliemc1996@gmail.com>
2018-10-23 01:25:16 -0400

    added images

---
+++ url_update.py
@@ -0,0 +1,28 @@
+"""
+updates url.txt for the open data labeling tool on shadysource.github.io
+"""
+import os
+
+try:
+    os.remove('url.txt')
+except:
+    pass
+dirs = os.listdir('data')
+dirs.sort()
+
+for i in dirs:
+    if '.' in i:
+        dirs.remove(i)
+
+with open('url.txt', 'w') as f:
+    for i in dirs:
+        f.write(i + ' ')
+    f.write('\n')
+
+    for i in dirs:
+        images = os.listdir(os.path.join('data',i))
+        for name in images:
+            if " " in name:
+                os.rename(os.path.join('data',i, name), os.path.join('data',i, name.replace(" ", "_")))
+            f.write('https://github.com/ksu-auv-team/makerfaire-2018-data/raw/master/data/'+i+'/'+name+' ')
+        f.write('\n')
